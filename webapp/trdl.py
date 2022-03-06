c6a8676dfd6336042ff1d75ba0dc18542e0e2e6c
chakri276 <chakradhara.konduru@gmail.com>
2022-03-07 00:52:27 +0100

    added kube, nginx, wsgi setup!!

--- webapp/trdl.py
+++ webapp/trdl.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import make_response
 
 app = Flask(__name__)
 
@@ -6,5 +7,11 @@ app = Flask(__name__)
 def main():
     return "42"
 
+@app.route('/<page_name>')
+def other_page(page_name):
+    response = make_response('The page named %s does not exist.' \
+                             % page_name, 404)
+    return response
+
 if __name__=="__main__":
-    app.run()
+    app.run(host='0.0.0.0',port=5000,debug=True)
